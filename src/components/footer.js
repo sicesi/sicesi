6d774fd2d9db34d8459029c047a96d162f5850ed
Alex Martin <alexmartinfr@me.com>
2021-05-20 21:29:23 +0200

    Fix footer SVG margins.

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ import GithubLogo from "./github.inline.svg"
 
 const Footer = () => (
   <footer className="mt-16 text-gray-100 w-full mt-16">
-    <BackgroundWaves className="-mx-8" />
+    <BackgroundWaves className="max-w-screen min-w-screen" />
     <div className="bg-gray-700">
       <div className="px-8 md:px-12 lg:px-16 py-8 max-w-4xl mx-auto my-0 flex justify-between items-center">
         <div className="flex">
