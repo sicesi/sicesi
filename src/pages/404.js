fa1dca0fa129a09b9ae26018e2a02d30030dfcc0
Alex Martin <alexmartinfr@me.com>
2021-05-20 17:34:47 +0200

    Update 404.js
    - Fix the flashing 404 message on first load.
    - Basic style
    - Link to homepage

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,28 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const browser = typeof window !== "undefined" && window
 
+const NotFoundPage = () => {
+  return (
+    browser && (
+      <Layout>
+        <SEO title="404: Page introuvable" />
+        <h1 className="text-sicesiGray text-9xl font-extrabold mt-16">
+          404<span className="text-sicesiOrange">.</span>
+        </h1>
+        <p className="text-xl">Cette page est introuvable</p>
+        <Link
+          to="/"
+          className="transition duration-150 ease-in-out inline-block bg-gradient-to-b from-sicesiOrange to-orange-600 opacity-90 hover:opacity-100 text-white font-bold px-4 py border-0 border-transparent text-base font-medium rounded-full hover:bg-orange-600 md:text-lg py-2 px-7"
+        >
+          Retour à l'accueil
+        </Link>
+      </Layout>
+    )
+  )
+}
 export default NotFoundPage
